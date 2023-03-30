45fc880cab03e1cf7b6da629e345c85fc507f0b8
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-03-30 11:01:51 +0200

    DRm teljes törlése

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 console.log('Hát itt nincs semmi')
 
-var currentURL = window.location.href;
 
-window.onload = function() {
-    var url = window.location.href;
-    if (url.indexOf('nightdream-development.github.io') === -1) {
-      document.getElementById("warning").style.display = "block";
-      console.log('ELBASZTAD LOPÓS GECI')
-      //Figyu lopjad amúgyis a felét a chatgpt csinálta hiszen drazox egy kibaszott retard js terén!
-    }
-    console.log(currentURL, 'Sikeres betöltés!')
-  };
+
+// Felesleges URL eltárolás DRM miatt!
+//var currentURL = window.location.href;
+
+// Felesleges szarságok XDDDDDDD
+
+//window.onload = function() {
+//    var url = window.location.href;
+//    if (url.indexOf('nightdream-development.github.io') === -1) {
+//      document.getElementById("warning").style.display = "block";
+//      console.log('ELBASZTAD LOPÓS GECI')
+//      //Figyu lopjad amúgyis a felét a chatgpt csinálta hiszen drazox egy kibaszott retard js terén!
+//    }
+//    console.log(currentURL, 'Sikeres betöltés!')
+//  };
